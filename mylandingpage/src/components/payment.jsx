f58ef1fb20a6c982ff5010c10474d7ad65e6bb12
Hardik <ihardik.112@gmail.com>
2024-11-15 12:19:32 +0530

    updated payment.jsx

--- mylandingpage/src/components/payment.jsx
+++ mylandingpage/src/components/payment.jsx
@@ -14,43 +14,51 @@ export const Payment = () => {
         <strong>IFSC Code:</strong> SBIN0002580
       </p>
 
-      <h3>Fee Structure</h3>
-      <table className="fee-structure-summary-table">
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Early Bird (INR)</th>
-            <th>Late (INR)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Students (UG/PG)</td>
-            <td>2000</td>
-            <td>2500</td>
-          </tr>
-          <tr>
-            <td>PhD/Post Doctoral Fellow/Research Scholar</td>
-            <td>2500</td>
-            <td>3000</td>
-          </tr>
-          <tr>
-            <td>Academia</td>
-            <td>4000</td>
-            <td>5000</td>
-          </tr>
-          <tr>
-            <td>Industries</td>
-            <td>6000</td>
-            <td>7000</td>
-          </tr>
-          <tr>
-            <td>Accompanying Person</td>
-            <td>2500</td>
-            <td>3000</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="fee-structure">
+            <h3>Fee Structure - From India(INR) </h3>
+            <table className="fee-structure-table">
+              <thead>
+                <tr>
+                  <th>Category</th>
+                  <th>Early Bird (INR)</th>
+                  <th>Late (INR)</th>
+                  <th>On Spot (INR)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Students (UG/PG)</td>
+                  <td>2500</td>
+                  <td>3000</td>
+                  <td>3500</td>
+                </tr>
+                <tr>
+                  <td>PhD/Post Doctoral Fellow/Research Scholar</td>
+                  <td>3500</td>
+                  <td>4000</td>
+                  <td>5000</td>
+                </tr>
+                <tr>
+                  <td>Faculty/Scientist</td>
+                  <td>5000</td>
+                  <td>6000</td>
+                  <td>7000</td>
+                </tr>
+                <tr>
+                  <td>Industries</td>
+                  <td>6000</td>
+                  <td>7000</td>
+                  <td>8000</td>
+                </tr>
+                <tr>
+                  <td>Accompanying Person</td>
+                  <td>2500</td>
+                  <td>3000</td>
+                  <td>3500</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
       <a
         href='/payment-page'
         className='btn btn-custom btn-lg page-scroll'
