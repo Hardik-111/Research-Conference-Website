9ef33e3c94b3a93349e0407625de26b3b03355e6
Hardik <ihardik.112@gmail.com>
2025-02-18 22:11:14 +0530

    modified footer

--- mylandingpage/src/components/footer.jsx
+++ mylandingpage/src/components/footer.jsx
@@ -5,7 +5,7 @@ export const Footer = (props) => {
       <div id='footer' style={{padding:"0rem"}}>
         <div className='container text-center' style={{ backgroundColor:'#2c2d31' ,width:'100%', height:'6%' , position:'absolute' ,margin:"0rem" }}>
           <p style={{ color:'white',  margin:'1rem' }}>
-            &copy; Copyright MNNIT 2025. All right Reserved by MNNIT Allahabad. Developed by <a href="https://www.linkedin.com/in/hardik-kumar-singh/" style={{ color:'yellow' }}>Hardik Kumar Singh</a>
+            &copy; Copyright MNNIT 2025. All right Reserved by MNNIT Allahabad. Developed by <a href="https://www.linkedin.com/in/hardik-kumar-singh/" target="_blank" style={{ color:'yellow' }}>Hardik Kumar Singh</a>
           </p>
         </div>
       </div>
