10d94ea3ad689f6b2bac4193ac98c695f99b64ad
Hardik_112 <89783619+Hardik-111@users.noreply.github.com>
2026-03-14 14:15:41 +0530

    Add middleware for maintenance mode response

---
+++ mylandingpage/src/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const maintenance = process.env.MAINTENANCE_MODE;
+
+  if (maintenance === "true") {
+    return new NextResponse(
+      "<h1 style='text-align:center;margin-top:100px'>Site Under Maintenance</h1>",
+      { headers: { "content-type": "text/html" } }
+    );
+  }
+
+  return NextResponse.next();
+}
